e2dd672b8e8dca88abf0c6bfbcc0d492a9e19367
alrom8 <alexandre.romeo.descartes@gmail.com>
2019-10-17 15:41:19 +0200

    ion

--- src/app/soumission-reponse/soumission-reponse.component.ts
+++ src/app/soumission-reponse/soumission-reponse.component.ts
@@ -1,23 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-soumission-reponse',
-  templateUrl: './soumission-reponse.component.html',
-  styleUrls: ['./soumission-reponse.component.css']
+  selector: "app-soumission-reponse",
+  templateUrl: "./soumission-reponse.component.html",
+  styleUrls: ["./soumission-reponse.component.css"]
 })
 export class SoumissionReponseComponent implements OnInit {
+  private motPropose: String;
 
-  private motPropose:String;
-
-  constructor(motPropose:String) {
+  constructor(motPropose: String) {
     this.motPropose = motPropose;
-   }
-
-  ngOnInit() {
   }
 
-  public estMotTrouve(motATrouver:String):Boolean{
+  ngOnInit() {}
+
+  public estMotTrouve(motATrouver: String): Boolean {
+    console.log("ok");
     return this.motPropose.toLowerCase() == motATrouver.toLowerCase();
   }
-
 }
