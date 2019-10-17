2333bad1c4c0fc550832cff64674d1f29a18c445
HajarBenaddi <hajar_anissa@hotmail.fr>
2019-10-17 17:23:50 +0200

    Pictionary-1001: grid Of Cards

--- src/app/themes/themes.component.ts
+++ src/app/themes/themes.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-themes',
@@ -7,6 +13,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./themes.component.css']
 })
 export class ThemesComponent implements OnInit {
+  tiles: Tile[] = [
+    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
+    {text: 'Five', cols: 1, rows: 1, color: '#DDCHF8'},
+  ];
 
   constructor() { }
 
