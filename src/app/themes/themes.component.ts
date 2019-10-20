7c3fa420a5eccd187628cd715b85be7589358e68
HajarBenaddi <hajar_anissa@hotmail.fr>
2019-10-20 19:47:58 +0200

    Pictionnary-1001: images fonctionnent dans assets

--- src/app/themes/themes.component.ts
+++ src/app/themes/themes.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 export interface Tile {
-  color: string;
+ 
   cols: number;
   rows: number;
   text: string;
   imageUrl: string;
   borderRadius: string;
-  fontSize: string;
-  fontFamily: string;
+  
 }
 
 @Component({
@@ -21,11 +20,12 @@ export class ThemesComponent implements OnInit {
 
   constructor() { 
      this.tiles = [
-       {text: 'Transports', cols: 1, rows: 1, color: '', fontSize: '18',fontFamily: 'Roboto' ,imageUrl:'ressources/images/1.jpg', borderRadius:'20px'},
-       {text: 'Fruits', cols: 1, rows: 1, color: 'lightgreen', fontSize: '18',fontFamily: 'Roboto' ,imageUrl:'../../ressources/images/2.jpg',borderRadius:'20px'},
-       {text: 'Jeux', cols: 1, rows: 1, color: 'lightpink', fontSize: '15',fontFamily: 'Roboto' ,imageUrl:'',borderRadius:'20px'},
-       {text: 'Animaux', cols: 1, rows: 1, color: '#DDBDF1', fontSize: '15',fontFamily: 'Roboto' ,imageUrl:'',borderRadius:'20px'},
-       {text: 'Plantes', cols: 1, rows: 1, color: '#DDCHF9', fontSize: '15',fontFamily: 'Roboto' ,imageUrl:'https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct',borderRadius:'20px'},
+       {text: 'Transports', cols: 1, rows: 1 ,imageUrl:'assets/images/5.jpg', borderRadius:'20px'},
+       {text: 'Fruits', cols: 1, rows: 1 ,imageUrl:'assets/images/2.jpg',borderRadius:'20px'},
+       {text: 'Jeux', cols: 1, rows: 1 ,imageUrl:'assets/images/3.jpg',borderRadius:'20px'},
+       {text: 'Animaux', cols: 1, rows: 1,imageUrl:'assets/images/4.jpg',borderRadius:'20px'},
+       {text: 'Plantes', cols: 1, rows: 1,imageUrl:'assets/images/6.jpg',borderRadius:'20px'},
+       {text: 'Ville', cols: 1, rows: 1,imageUrl:'assets/images/1.jpg',borderRadius:'20px'},
      ];
   }
 
