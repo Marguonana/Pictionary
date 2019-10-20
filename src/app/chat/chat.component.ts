03f8be9989a06f2b000ed2fda71e165c1bdc7e60
MarguoNana <n.marguerite@live.fr>
2019-10-20 21:12:15 +0200

    Pictionary-F103 : Générer les zones de saisies

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -8,23 +8,17 @@ import { Component, OnInit } from "@angular/core";
 export class ChatComponent implements OnInit {
   pseudo: String;
   message: String;
-  //messages: Array<String>;
   messages: String;
 
   constructor() {
-    this.pseudo = "pse";
+    this.pseudo = sessionStorage.getItem('pseudo') ? sessionStorage.getItem('pseudo') : 'randomUser';
     this.message = "";
-    //this.messages = new Array<String>();
     this.messages="";
   }
 
   ngOnInit() {}
 
   posterMessage() {
-    /*console.log(this.pseudo + " : " + this.message);
-    this.messages.push(this.pseudo + " : " + this.message + "\n");
-    console.log(this.messages);
-    this.message = "";*/
     if(this.message != ""){
       this.messages+= this.pseudo + " : " + this.message + "\n";
       this.message="";
