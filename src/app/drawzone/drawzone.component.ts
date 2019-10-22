3a729c7b6c832aa0c8e01df2669cd9e0979bc181
MarguoNana <n.marguerite@live.fr>
2019-10-22 13:35:34 +0200

    Pictionary-F201: Choisir de passer en mode gomme

--- src/app/drawzone/drawzone.component.ts
+++ src/app/drawzone/drawzone.component.ts
@@ -12,16 +12,19 @@ export class DrawzoneComponent implements OnInit {
   private isDrawing : boolean = false;  
   private ctx: CanvasRenderingContext2D;
   callibrage: { 'black': { 'coordX': number; 'coordY': number; }[]; 'white': any[]; }[];
-  private pen: any;
+  pen;
   private pointer : string;
 
   ngOnInit(): void {
+    this.pen = {};
     this.ctx = this.canvas.nativeElement.getContext('2d');
     this.ctx.imageSmoothingQuality = 'high';
     this.ctx.lineJoin = 'round';
+    this.ctx.lineWidth = 3;
     this.ctx.fillStyle = 'black';
     this.callibrage = [{'black': [{'coordX': -10, 'coordY':5}],
                         'white': [] }];
+    this.pen.cursor = 'url(../../assets/images/cursor/Pencil_black.png) 0 15,auto';
   }
   
   animate(): void {}
@@ -39,7 +42,6 @@ export class DrawzoneComponent implements OnInit {
   drawing(event) : void{
     if(this.isDrawing){
       this.ctx.beginPath();
-      this.ctx.lineWidth = 3;
       this.ctx.lineCap = 'round';
       this.ctx.moveTo(event.clientX-event.currentTarget.offsetLeft,event.clientY-event.currentTarget.offsetTop);
       this.ctx.lineTo(event.clientX-event.currentTarget.offsetLeft,event.clientY-event.currentTarget.offsetTop);
@@ -49,16 +51,21 @@ export class DrawzoneComponent implements OnInit {
     }
   }
 
-  selectItem(item) : string {
-    let style = 'url(../../ressources/images/Pencil_black.png) 0 15,auto'
+  selectItem(item) : void {
     switch(item){
       case 'eraser':
-        style = 'url(../../ressources/images/Eraser.jpeg) 0 15,auto';
+        this.ctx.strokeStyle = 'white';
+        this.ctx.lineWidth = 10;
+        this.pen.cursor = 'url(../../assets/images/cursor/Eraser.png) 0 15,auto';
+        break;
+      case 'blackPen':
+        this.ctx.strokeStyle = 'black';
+        this.ctx.lineWidth = 3;
+        this.pen.cursor = 'url(../../assets/images/cursor/Pencil_black.png) 0 15, auto';
         break;
       default: 
         break;
     }
-    return style;
   }
 
 
