40686f1e4f608037dc5081c6977c1f0fde6e6732
MarguoNana <n.marguerite@live.fr>
2019-10-25 11:21:19 +0200

    Pictionary-B101: Accéder à la base de données avec les thèmes

--- src/app/themes/themes.component.ts
+++ src/app/themes/themes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; 
+import { DataService } from '../data.service';
 
 export interface Tile {
  
@@ -19,14 +20,14 @@ export interface Tile {
 export class ThemesComponent implements OnInit {
    tiles: Tile[] ;
 
-  constructor(private router: Router) { 
+  constructor(private router: Router,private dataService: DataService) { 
      this.tiles = [
        {text: 'Transports', cols: 1, rows: 1 ,imageUrl:'assets/images/5.jpg', borderRadius:'20px'},
        {text: 'Fruits', cols: 1, rows: 1 ,imageUrl:'assets/images/2.jpg',borderRadius:'20px'},
        {text: 'Jeux', cols: 1, rows: 1 ,imageUrl:'assets/images/3.jpg',borderRadius:'20px'},
        {text: 'Animaux', cols: 1, rows: 1,imageUrl:'assets/images/4.jpg',borderRadius:'20px'},
        {text: 'Plantes', cols: 1, rows: 1,imageUrl:'assets/images/6.jpg',borderRadius:'20px'},
-       {text: 'Ville', cols: 1, rows: 1,imageUrl:'assets/images/1.jpg',borderRadius:'20px'},
+       {text: 'Villes', cols: 1, rows: 1,imageUrl:'assets/images/1.jpg',borderRadius:'20px'},
      ];
   }
 
@@ -34,8 +35,14 @@ export class ThemesComponent implements OnInit {
   }
 
   onClick(tile) : void {
+    this.dataService.setParams(tile.text.toLowerCase(),'choixTheme');
+    this.dataService.sendGetRequest().subscribe((data: ArrayBuffer)=>{
+      console.log(data);
+    })
+    // tuto : https://www.techiediaries.com/angular-by-example-httpclient-get/
     this.router.navigate(['/play',{'theme': tile.text}]);
     // console.log ("welcome"+ tile.text);
 
   }
+
 }
