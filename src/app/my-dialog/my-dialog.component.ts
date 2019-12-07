7dcfbb20a2ec3692a1ecbff88ee5b64bdd7efea8
HajarBenaddi <hajar_anissa@hotmail.fr>
2019-12-07 22:48:46 +0100

    Pictionary-F205: Correction erreurs

--- src/app/my-dialog/my-dialog.component.ts
+++ src/app/my-dialog/my-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit , Inject } from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 
+
 @Component({
   selector: 'app-my-dialog',
   templateUrl: './my-dialog.component.html',
@@ -9,12 +10,17 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 export class MyDialogComponent  {
 
   modalTitle: string;
+  modalWord:Array<string>;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+ 
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,) {
+  
   this.modalTitle = data.title;
   console.log(data)
   }
 
+  
   
 
 }
